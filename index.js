9190afa350069e6f00199ab969689133327cc4ff
jenishajeya <jenishajeya@icloud.com>
2023-04-19 15:18:10 +0200

    k

--- index.js
+++ index.js
@@ -5,7 +5,7 @@ const products = require("./routes/product");
 const cors = require("cors");
 const ProductModel = require("./models/productModel");
 
-const HOST = process.env.HOST || "mongodb://127.0.0.1/DatabaseName";
+const HOST = process.env.HOST || "mongodb+srv://jeyajenisha:<password>@clusterz.vyaygvd.mongodb.net/test";
 const PORT = process.env.PORT || 3001;
 
 const mongoose = require("mongoose");
